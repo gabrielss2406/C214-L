f51c063be377f4f06402c31d60e958bb0fc29122
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-08-30 10:52:40 -0300

    :sparkles: feat: ToDoList

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,57 @@
-type Task = {
-  name: string;
-  schedule: Date;
-};
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import readline from "readline";
+import { promisify } from "util";
+import { Task, ToDoList } from "./ToDoList";
 
-class ToDoListManager {
-  private tasks: Task[] = [];
-  constructor() {}
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  insertTask(task: Task) {
-    this.tasks.push(task);
+const userInput = promisify(reader.question).bind(reader);
+
+async function getUserInput(prompt: string): Promise<any> {
+  return await userInput(prompt);
+}
+
+const toDoList = new ToDoList();
+
+async function addNewTask() {
+  const newTask: Task = {
+    title: "",
+    description: "",
+    targetDate: "",
+    type: "",
+    priority: "",
+  };
+  newTask.title = await getUserInput("Digite o título da tarefa: ");
+  newTask.description = await getUserInput("Digite a descrição da tarefa: ");
+  newTask.targetDate = await getUserInput("Digite a data limite da tarefa: ");
+
+  toDoList.add(newTask);
+}
+
+async function startTodoList() {
+  const userChoise = await getUserInput(
+    "Digite 1 para adicionar uma tarefa, 2 para visualizar as tarefas ou 0 para sair: ",
+  );
+
+  switch (userChoise) {
+    case "0":
+      reader.close();
+      return;
+    case "1":
+      await addNewTask();
+      console.log("TAREFAS", toDoList.getTasks());
+      startTodoList();
+      return;
+    case "2":
+      console.log("TAREFAS", toDoList.getTasks());
+      startTodoList();
+      return;
+    default:
+      reader.close();
   }
 }
 
-const task: Task = {
-  name: "aula 03",
-  schedule: new Date("2024-08-23"),
-};
-
-const taskManager = new ToDoListManager();
-taskManager.insertTask(task);
+startTodoList();
